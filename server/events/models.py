03a93efefe637c10a103b36f3c678d7e80aa769a
nickpo <nickpeteromalley@gmail.com>
2016-06-05 10:28:04 -0400

    updated views and models

--- server/events/models.py
+++ server/events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    date = models.CharField(max_length=40)
+    board = models.CharField(max_length=64)
+    meeting_type = models.CharField(max_length=64)
+    status = models.CharField(max_length=50)
+    location = models.CharField(max_length=128)
--- server/events/views.py
+++ server/events/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from lxml import html
+import requests
 
 # Create your views here.
+def index_test(req):
+    page = requests.get('http://somervillecityma.iqm2.com/Citizens/calendar.aspx')
+    tree = html.fromstring(page.content)
+    events = tree.xpath('//*[@id="ContentPlaceholder1_pnlMeetings"]/div/div[1]/div[2]/a/@title')
+
+    z = []
+
+    # this split is required -- the list items get cut off otherwise
+    a = [b.split('\r') for b in events]
+    for c in a:
+        z.append('\n{}\n'.format('-'*5))
+        for d in c:
+            z.append(d.split())
+
+    return(HttpResponse(z))
