d93737a91cbfdfe8607ccf22d546ce41db3c636d
nickpo <nickpeteromalley@gmail.com>
2016-06-05 20:34:43 -0400

    updated lxml-test

--- server/events/scripts/lxml-test.py
+++ server/events/scripts/lxml-test.py
@@ -1,26 +1,38 @@
+from datetime import datetime
 from lxml import html
 import requests
+
+BASE_URL = 'http://www.somervillema.gov'
 
 #page = requests.get('http://www.somervillema.gov/government/public-minutes')
 with open('test.html', 'r') as filename:
     data = filename.read()
-
 tree = html.fromstring(data)
-titles = tree.xpath('/html/body/center/table/tbody/tr[2]/td/table/tbody/tr[1]/td[3]/table/tbody/tr[2]/td[1]/div/div[3]/table[2]/tbody/tr[1]/td[2]/a[@href]')
-
-print titles
 
 
+def get_date(tr):
+    xpath = ('/html/body/center/table/tbody/tr[2]/td/table/tbody/tr[1]/td[3]'
+             '/table/tbody/tr[2]/td[1]/div/div[3]//tbody/tr[{}]/td[1]/span/text()'.format(tr))
+    date = tree.xpath(xpath)
+
+    return datetime.strptime(str(date[0]), "%b %d, %Y")
 
 
-'''
-y = [x.split('\r') for x in titles]
+def get_link(tr):
+    xpath = ('/html/body/center/table/tbody/tr[2]/td/table/tbody/tr[1]/td[3]'
+             '/table/tbody/tr[2]/td[1]/div/div[3]//tbody/tr[{}]/td[2]/a/@href'.format(tr))
+    value = tree.xpath(xpath)
 
-for z in y:
-    print '\n{}\n'.format('-'*5)
-    for a in z:
-        print a.strip()
-'''
-#/html/body/center/table/tbody/tr[2]/td/table/tbody/tr[1]/td[3]/table/tbody/tr[2]/td[1]/div/div[3]/table[2]/tbody/tr[1]/td[2]
+    return value
 
-#/html/body/center/table/tbody/tr[2]/td/table/tbody/tr[1]/td[3]/table/tbody/tr[2]/td[1]/div/div[3]/table[2]/tbody/tr[2]/td[2]
+
+i = 1
+date_time = get_date(i)
+while date_time > datetime.now():
+    link = get_link(i)
+    print "Incremented! Here: {} {}".format(date_time, link)
+    print "New URL: {}".format(BASE_URL + link[0])
+
+    i += 1
+    date_time = get_date(i)
+    
